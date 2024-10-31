25f11e90e634bec98fbc2b39d04b279516c246f7
dikadik22 <dikafahrizal121@gmail.com>
2024-10-31 15:04:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function showPage(pageId) {
     const pages = document.querySelectorAll('.page');
-    pages.forEach(page => page.classList.remove('active'));
+    pages.forEach(page => {
+        page.classList.remove('active'); // Menghapus kelas aktif dari semua section
+    });
 
     const activePage = document.getElementById(pageId);
-    activePage.classList.add('active');
+    activePage.classList.add('active'); // Menambahkan kelas aktif ke section yang dipilih
 }
